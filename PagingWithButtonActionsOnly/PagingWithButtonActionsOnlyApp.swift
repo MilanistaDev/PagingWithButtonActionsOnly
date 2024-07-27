9ba994d1646423ee376b6f747aef52751459ab18
Takuya Aso <milanista224@gmail.com>
2024-07-27 23:56:11 +0900

    Initial Commit

---
+++ PagingWithButtonActionsOnly/PagingWithButtonActionsOnlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PagingWithButtonActionsOnlyApp.swift
+//  PagingWithButtonActionsOnly
+//
+//  Created by Takuya Aso on 2024/07/27.
+//
+
+import SwiftUI
+
+@main
+struct PagingWithButtonActionsOnlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
